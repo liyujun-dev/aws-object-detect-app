742f59547358df0ef9a0610bf3b91140ae019d32
Suricatta1998 <suricatta1998@outlook.com>
2021-08-09 14:23:17 +0000

    新增测试detect api脚本

---
+++ test_apis/test_detect_picture_api.py
@@ -0,0 +1,11 @@
+import requests
+
+api = 'https://7pa6zs0wm6.execute-api.us-east-1.amazonaws.com'
+detect_api = f'{api}/detect'
+key = '9024592fadae45eeaff4b68d71493748.jpg'
+res = requests.get(detect_api, params={
+  'key': key
+})
+if res.status_code == 200:
+  print(res.json())
+  
