c7a40b9724cc7ae22cb9a6770b076600e4a55ea1
Suricatta1998 <suricatta1998@outlook.com>
2021-08-12 14:08:27 +0000

    修改DIRECT_URL

--- website/src/constants.js
+++ website/src/constants.js
@@ -12,7 +12,7 @@ const ISS_URL = "";
 const COGNITO_DOMAIN = "";
 
 // Cognito URL 定义
-const DIRECT_URL = (new URL(location)).origin;
+const DIRECT_URL = `${(new URL(location)).origin}/index.html`;
 const LOGIN_URL = `${COGNITO_DOMAIN}/login?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${DIRECT_URL}`;
 const USER_INFO_URL = `${COGNITO_DOMAIN}/oauth2/userInfo`;
 const LOGOUT_URL = `${COGNITO_DOMAIN}/logout?client_id=${CLIENT_ID}&logout_uri=${DIRECT_URL}`;
